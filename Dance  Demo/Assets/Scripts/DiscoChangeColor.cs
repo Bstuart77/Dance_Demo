c783186d2e46d38a0fd7116380cdc0795caf5584
Brian <bstuart777@gmail.com>
2020-11-06 03:28:33 -0500

    script to change the color of floor every 2 seconds

---
+++ Dance  Demo/Assets/Scripts/DiscoChangeColor.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiscoChangeColor : MonoBehaviour
+{
+    public Light light;
+    private float timeLeft = 1f;
+
+    private void Start()
+    {
+        StartCoroutine(TimeCheck());
+        light = GetComponent<Light>();
+    }
+
+    IEnumerator TimeCheck()
+    {
+        while (timeLeft > 0)
+        {
+            light.color = Color.HSVToRGB(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f));
+            yield return new WaitForSeconds(1.5f);
+        }
+    }
+}
