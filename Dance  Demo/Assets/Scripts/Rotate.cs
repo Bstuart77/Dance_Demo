38d820659c484f13bcc47552f1d6304f269cf29e
Brian <bstuart777@gmail.com>
2021-01-01 19:32:28 -0500

    added audio source that controls the speed of lights

--- Dance  Demo/Assets/Scripts/Rotate.cs
+++ Dance  Demo/Assets/Scripts/Rotate.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Rotate : MonoBehaviour
 {
+    public AudioSource audio;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -12,7 +14,7 @@ public class Rotate : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
-        transform.Rotate(0, 45 * Time.deltaTime, 0);
+    { 
+        transform.Rotate(0, 45 * Time.deltaTime * audio.volume * 8, 0);
     }
 }
