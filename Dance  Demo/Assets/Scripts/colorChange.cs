aeb4e9708c40b03101085666d9079cd97a4613b0
Brian <bstuart777@gmail.com>
2021-01-01 21:20:42 -0500

    added script that changes floor colors of children every 1 second

---
+++ Dance  Demo/Assets/Scripts/colorChange.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class colorChange : MonoBehaviour
+{
+    private float time = 1f;
+    public GameObject[] floors;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(ChangeColor());
+    }
+
+    IEnumerator ChangeColor()
+    {
+        while(time > 0f)
+        {
+            Color newColor = new Color(Random.Range(0F, 1F), Random.Range(0, 1F), Random.Range(0, 1F));
+            print(newColor);
+            // apply it on current object's material
+            foreach(GameObject go in floors)
+            {
+                go.GetComponent<Renderer>().material.color = newColor;
+            }
+           yield return new WaitForSeconds(1f);
+
+        }
+    }
+}
